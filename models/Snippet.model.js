e217423cd6a90eac38dbe47b0627a1dd4dc6df01
Ivy <yunjilim@gmail.com>
2019-09-05 14:50:09 -0700

    converted all to postgresql

--- models/Snippet.model.js
+++ models/Snippet.model.js
@@ -1,10 +1,11 @@
-const fs = require('fs').promises;
+// const fs = require('fs').promises;
 const path = require('path');
-const shortid = require('shortid');
+const format = require('pg-format');
+// const shortid = require('shortid');
 const db = require('../db');
 const { readJSONFromDB, writeJSONToDB } = require('../utils/db-utils');
 const ErrorWithHTTPStatus = require('../utils/errorWithHTTPStatus');
-const dbpath = path.join(__dirname, '..', 'db', 'snippets.json');
+// const dbpath = path.join(__dirname, '..', 'db', 'snippets.json');
 
 /**
  * @typedef {Object} Snippet
@@ -25,7 +26,9 @@ const dbpath = path.join(__dirname, '..', 'db', 'snippets.json');
  */
 exports.insert = async ({ author, code, title, description, language }) => {
   try {
-    return db.query(`INSERT INTO snippet (code, title, description, author, language) VALUES ($1, $2, $3, $4, $5)`, [code, title, description, author, language]);
+    if (!author || !code || !title || !description || !language) throw new ErrorWithHTTPStatus('Missing properties', 400);
+    const result = await db.query(`INSERT INTO snippet (code, title, description, author, language) VALUES ($1, $2, $3, $4, $5) RETURNING *`, [code, title, description, author, language]);
+    return result.rows[0];
     // if (!author || !code || !title || !description || !language) throw new ErrorWithHTTPStatus('Missing Properties', 400);
     // const snippets = JSON.parse(await fs.readFile(dbpath));
     // snippets.push({
@@ -55,8 +58,13 @@ exports.insert = async ({ author, code, title, description, language }) => {
  */
 exports.select = async (query = {}) => {
   try {
-    const result = await db.query('SELECT * FROM snippet');
-    return result.rows;
+    const clauses = Object.keys(query).map((key, i) => `%I = $${i + 1}`).join(' AND ');
+    const formattedSelect = format(`SELECT * FROM snippet ${clauses.length ? `WHERE ${clauses}` : ''}`, ...Object.keys(query));
+    const results = await db.query(formattedSelect, Object.values(query));   
+    return results.rows;
+    // const result = await db.query('SELECT * FROM snippet');
+    // return result.rows;
+    
     // const snippets = JSON.parse(await fs.readFile(dbpath));
     // const filtered = snippets.filter(snippet => Object.keys(query).every(key => query[key] === snippet[key]));
     // return filtered;
@@ -72,36 +80,41 @@ exports.select = async (query = {}) => {
  */
 exports.update = async (id, newData) => {
   // TODO: error on Id not found
+  if (!id) throw ErrorWithHTTPStatus('Invalid Id', 400);
   try {
-    const result = await db.query(`SELECT id from snippet WHERE id = $1`, [id]);
-    if (result.rows.length === 0) throw new ErrorWithHTTPStatus('Snippet Does not Exist!', 400);
-    Object.keys(newData).forEach(async key => {
-      try {
-        await db.query(`UPDATE snippet SET $1 = $2 WHERE id = $3`, [key, newData[key], id]);
-      } catch (err) {
-        console.error(err);
-      }
-    });
-    // const snippets = await readJSONFromDB('snippets');
-    // let foundId = false;
-    // const updatedSnippets = snippets.map(snippet => {
-    //   if (snippet.id !== id) return snippet;
-    //   foundId = true;
-    //   Object.keys(newData).forEach(key => {
-    //     if (key in snippet) {
-    //       snippet[key] = newData[key];
-    //     } else {
-    //       throw new ErrorWithHTTPStatus('Key does not Exist!', 400);
-    //     }
-    //   });
-    //   if (!foundId) throw new ErrorWithHTTPStatus('Id not found!', 400);
-    //   return snippet;
-    // });
-    // return writeJSONToDB('snippets', updatedSnippets);
-  } catch (error) {
-    throw new ErrorWithHTTPStatus('Database Error!');
+    const { author, code, title, description, language } = newData;
+    await db.query(`UPDATE snippet SET 
+      author = COALESCE($2, author),
+      code = COALESCE($3, code),
+      title = COALESCE($4, title),
+      description = COALESCE($5, description),
+      language = COALESCE($6, language)
+    WHERE id = $1`,
+    [id, author, code, title, description, language]);
+  } catch (err) {
+    throw err;
   }
 };
+// const snippets = await readJSONFromDB('snippets');
+// let foundId = false;
+// const updatedSnippets = snippets.map(snippet => {
+//   if (snippet.id !== id) return snippet;
+//   foundId = true;
+//   Object.keys(newData).forEach(key => {
+//     if (key in snippet) {
+//       snippet[key] = newData[key];
+//     } else {
+//       throw new ErrorWithHTTPStatus('Key does not Exist!', 400);
+//     }
+//   });
+//   if (!foundId) throw new ErrorWithHTTPStatus('Id not found!', 400);
+//   return snippet;
+// });
+// return writeJSONToDB('snippets', updatedSnippets);
+// } catch (error) {
+//   throw new ErrorWithHTTPStatus('Database Error!');
+// }
+// };
 
 /**
  * Deletes a snippet with a given ID
