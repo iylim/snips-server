f176e1b40d5a5f1b532a86fdd54eebc3515cd80c
Ivy <yunjilim@gmail.com>
2019-09-09 11:26:32 -0700

    udated database url

--- db/index.js
+++ db/index.js
@@ -1,11 +1,7 @@
 require('dotenv').config();
 const pg = require('pg');
 
-const { DB_HOST, DB_PASSWORD, DB_USER, DB_PORT, DB_DATABASE, DATABASE_URL } = process.env;
-
-// const connectionString = (`postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_DATABASE}`);
-const connectionString = (`${DATABASE_URL}`);
-const pool = new pg.Pool({ connectionString });
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 
 
 // pool.query('SELECT * FROM snippet')
