d30cf53969b9075938cd6858e5587bc195abea3a
Ivy <yunjilim@gmail.com>
2019-09-25 14:13:35 -0700

    added file name

--- controllers/snippets.controller.js
+++ controllers/snippets.controller.js
@@ -1,5 +1,5 @@
 const Snippet = require('../models/Snippet.model');
-const ErrorWithHttpStatus = require('../utils/ErrorWithHTTPStatus');
+const ErrorWithHttpStatus = require('../utils/ErrorWithHTTPStatus.js');
 exports.createSnippet = async (request, response, next) => {
   try {
     // create a snippet
--- models/Snippet.model.js
+++ models/Snippet.model.js
@@ -1,6 +1,6 @@
 const format = require('pg-format');
 const db = require('../db');
-const ErrorWithHttpStatus = require('../utils/ErrorWithHTTPStatus');
+const ErrorWithHttpStatus = require('../utils/ErrorWithHTTPStatus.js');
 
 /**
  * a snippet object
